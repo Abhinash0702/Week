ddca4c29c8f2defe165bbf2cfefb47913f7c4581
Abhinash Shukla <abhinashshukla2002@gmail.com>
2025-01-14 14:58:32 +0530

    Program 1

---
+++ Level 01/Lv01_Program01.java
@@ -0,0 +1,10 @@
+public class Lv01_Program01 {
+    public static void main(String[] args) {
+        int birthYear = 2000; // Harry's birth year
+        int currentYear = 2024; // Current year
+
+        int age = currentYear - birthYear; // Calculate age
+
+        System.out.println("Harry's age in " + currentYear + " is " + age);
+    }
+}
